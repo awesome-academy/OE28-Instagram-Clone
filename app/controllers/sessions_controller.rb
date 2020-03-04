5e8e860bc47c6da3b3d9959421268f8d48bef0e5
Việt Hoàng <justalak29@gmail.com>
2020-03-04 22:49:21 +0700

    login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+    redirect_to current_user if user_signed_in?
+  end
+
+  def create
+    user = User.find_by email: params[:session][:email]
+    if user&.authenticate(params[:session][:password])
+      log_in_if_activated user
+    else
+      flash.now[:danger] = t ".invalid_message"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if user_signed_in?
+    redirect_to login_path
+  end
 end
